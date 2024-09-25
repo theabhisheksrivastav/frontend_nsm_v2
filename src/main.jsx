3db80cb592e8ae5883411fdc8bc20054cea79984
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-09-25 20:00:44 +0530

    test bug 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import {Home, LoginPage, RegisterPage, OtpPage} from './pages'
+import {Home, RegisterPage, OtpPage} from './pages'
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route key="app" path="/" element={<App />} >
